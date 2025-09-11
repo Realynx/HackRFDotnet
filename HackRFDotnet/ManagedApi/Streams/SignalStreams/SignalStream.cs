35b3ea67c5f0a75eb37a9a2d5a174d5ed32f69d0
ScrubN <72096833+ScrubN@users.noreply.github.com>
2025-09-11 14:19:04 -0400

    Fix some signal stream issues

--- HackRFDotnet/ManagedApi/Streams/SignalStreams/SignalStream.cs
+++ HackRFDotnet/ManagedApi/Streams/SignalStreams/SignalStream.cs
@@ -1,4 +1,5 @@
-﻿using System.Numerics;
+﻿using System.Buffers;
+using System.Numerics;
 
 using HackRFDotnet.ManagedApi.SignalProcessing;
 using HackRFDotnet.ManagedApi.Types;
@@ -7,7 +8,7 @@ using NAudio.Wave;
 
 namespace HackRFDotnet.ManagedApi.Streams.SignalStreams;
 public class SignalStream : ISampleProvider, IDisposable {
-    public WaveFormat WaveFormat { get; protected set; }
+    public WaveFormat? WaveFormat { get; protected set; }
 
     protected readonly bool _keepOpen;
     protected readonly RfDeviceStream _rfDeviceStream;
@@ -15,24 +16,21 @@ public class SignalStream : ISampleProvider, IDisposable {
     protected RadioBand _center = RadioBand.FromMHz(94.7f);
     protected RadioBand _bandwith = RadioBand.FromKHz(200);
 
-    protected FilterProcessor _filterProcessor;
+    protected FilterProcessor? _filterProcessor;
 
     public SignalStream(RfDeviceStream deviceStream, bool keepOpen = true) {
         _keepOpen = keepOpen;
         _rfDeviceStream = deviceStream;
     }
 
-    public void SetBand(RadioBand center, RadioBand bandwith) {
+    public void SetBand(RadioBand center, RadioBand bandwidth) {
         _center = center;
-        _bandwith = bandwith;
+        _bandwith = bandwidth;
 
-        _filterProcessor = new FilterProcessor(_rfDeviceStream.SampleRate, center, bandwith);
+        _filterProcessor = new FilterProcessor(_rfDeviceStream.SampleRate, center, bandwidth);
     }
 
     protected int ReadSpan(Span<Complex> iqPairs) {
-        while (iqPairs.Length > _rfDeviceStream.BufferLength) {
-        }
-
         var readBytes = _rfDeviceStream.ReadBuffer(iqPairs);
         _filterProcessor.ApplyFilter(iqPairs);
 
@@ -40,14 +38,19 @@ public class SignalStream : ISampleProvider, IDisposable {
     }
 
     public virtual int Read(float[] buffer, int offset, int count) {
-        var iqBuffer = new Complex[count];
-        ReadSpan(iqBuffer);
+        var iqBuffer = ArrayPool<Complex>.Shared.Rent(count);
+        try {
+            ReadSpan(iqBuffer.AsSpan(0, count));
 
-        for (var i = 0; i < count; i++) {
-            buffer[offset + i] = (float)iqBuffer[i].Real;
+            for (var i = 0; i < count; i++) {
+                buffer[offset + i] = (float)iqBuffer[i].Real;
+            }
+
+            return count;
         }
-
-        return iqBuffer.Length;
+        finally {
+            ArrayPool<Complex>.Shared.Return(iqBuffer);
+        }
     }
 
     public void Dispose() {
