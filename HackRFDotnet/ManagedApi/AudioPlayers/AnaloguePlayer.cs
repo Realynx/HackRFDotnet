973ada441824e41a7b86832141e34871fbabefd3
PoofImaFox <16710818+PoofImaFox@users.noreply.github.com>
2025-09-11 15:20:52 -0400

    chore: better player class

--- HackRFDotnet/ManagedApi/AudioPlayers/AnaloguePlayer.cs
+++ HackRFDotnet/ManagedApi/AudioPlayers/AnaloguePlayer.cs
@@ -7,9 +7,10 @@ using NAudio.Wave.SampleProviders;
 namespace HackRFDotnet.ManagedApi.AudioPlayers {
     public class AnaloguePlayer {
         private readonly SignalStream _sampleDeModulator;
-
+        private SampleToWaveProvider16 _sampleToWaveProvider16;
         private BufferedWaveProvider _waveProvider;
         private WaveOutEvent _waveOut;
+        private Thread _playbackThread;
 
         public AnaloguePlayer(SignalStream signalStream) {
             _sampleDeModulator = signalStream;
@@ -24,20 +25,20 @@ namespace HackRFDotnet.ManagedApi.AudioPlayers {
             _waveOut.Init(_waveProvider);
             _waveOut.Play();
 
-            new Thread(() => StreamIqFrames(centerOffset, bandwith, audioRate)).Start();
+            var resampler = new WdlResamplingSampleProvider(_sampleDeModulator, audioRate);
+            _sampleToWaveProvider16 = new SampleToWaveProvider16(resampler);
+
+            _playbackThread = new Thread(() => StreamIqFrames(centerOffset, bandwith));
+            _playbackThread.Start();
         }
 
-        private void StreamIqFrames(RadioBand centerOffset, RadioBand bandwith, int audioRate = 44100) {
+        private void StreamIqFrames(RadioBand centerOffset, RadioBand bandwith) {
             _sampleDeModulator.SetBand(centerOffset, bandwith);
-
-            var resampler = new WdlResamplingSampleProvider(_sampleDeModulator, audioRate);
-            var pcmProvider = new SampleToWaveProvider16(resampler);
-
-            var waveProviderBuffer = new byte[8192];
+            var waveProviderBuffer = new byte[10_000];
 
             int read;
             while (true) {
-                while ((read = pcmProvider.Read(waveProviderBuffer, 0, waveProviderBuffer.Length)) > 0) {
+                while ((read = _sampleToWaveProvider16.Read(waveProviderBuffer, 0, waveProviderBuffer.Length)) > 0) {
                     _waveProvider.AddSamples(waveProviderBuffer, 0, read);
                 }
             }
