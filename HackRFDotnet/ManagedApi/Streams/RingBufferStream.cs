253bc498b5937cee4730a3189f82995234fe1ed1
ScrubN <72096833+ScrubN@users.noreply.github.com>
2025-09-11 13:24:10 -0400

    Create RingBufferStream

---
+++ HackRFDotnet/ManagedApi/Streams/RingBufferStream.cs
@@ -0,0 +1,49 @@
+﻿using HackRFDotnet.ManagedApi.Types;
+
+namespace HackRFDotnet.ManagedApi.Streams;
+
+internal sealed class RingBufferStream : Stream
+{
+    private readonly RingBuffer<byte> _ringBuffer;
+
+    public RingBufferStream(RingBuffer<byte> ringBuffer)
+    {
+        _ringBuffer = ringBuffer;
+    }
+
+    public override void Flush() { }
+
+    public override int Read(byte[] buffer, int offset, int count)
+    {
+        return _ringBuffer.Read(buffer.AsSpan(offset, count));
+    }
+
+    public override long Seek(long offset, SeekOrigin origin)
+    {
+        throw new NotSupportedException();
+    }
+
+    public override void SetLength(long value)
+    {
+        throw new NotSupportedException();
+    }
+
+    public override void Write(byte[] buffer, int offset, int count)
+    {
+        _ringBuffer.Write(buffer.AsSpan(offset, count));
+    }
+
+    public override bool CanRead => true;
+
+    public override bool CanSeek => false;
+
+    public override bool CanWrite => true;
+
+    public override long Length => _ringBuffer.Count;
+
+    public override long Position
+    {
+        get => 0;
+        set => throw new NotSupportedException();
+    }
+}
